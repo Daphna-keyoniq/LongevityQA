942b28f866209ce013e99a1006f539df7d7bdce2
daphna-keyoniq <daphna.keidar@keyoniq.com>
2025-08-06 07:19:41 +0000

    add questions

--- app/src/components/PredefinedQA.tsx
+++ app/src/components/PredefinedQA.tsx
@@ -5,16 +5,24 @@ import "./../styles/PredefinedQA.css"; // Import styles for the component
 const predefinedQAData = [
   {
     question: "What is longevity?",
-    answer: "Longevity refers to the length of time that an individual lives."
+    answer: "Longevity care is a field of medicine that focuses on extending the healthy lifespan of individuals, with a proactive approach to healthcare. The focus being on preventing diseases, delaying the process of aging, and promoting overall well-being, rather than treating diseases after they occur."
   },
   {
-    question: "How can I improve my longevity?",
-    answer: "You can improve your longevity by maintaining a healthy lifestyle, exercising, and eating a balanced diet."
+    question: "How is healthspan?",
+    answer: "Healthspan is the number of years in which one lives in good health and has a high quality of life, free from chronic diseases and disabilities. It emphasizes living better and healthier for longer."
   },
   {
-    question: "What are the key factors affecting longevity?",
-    answer: "Key factors include genetics, lifestyle, environment, and access to healthcare."
-  }
+    question: "What main longevity treatments are offered?",
+    answer: "Longevity treatments include lifestyle modifications, nutritional interventions, exercise programs, supplements, and advanced medical therapies. Some longevity medications exist, often in the form of repurposed drugs, with new evidence coming out that these medications also have a positive effect of slowing down aging."
+  },
+  {
+    question: "What are repurposed longevity drugs?",
+    answer: "Repurposed longevity drugs, such as metformin and rapamycin, refer to medications that were developed and approved for one disease, and later found to also be useful as longevity medications. Resaerch on these drugs as longevity treatments is larger new, and often still ongoing, but they have shown promise in extending lifespan and healthspan in various studies."
+  }, 
+  {
+    question: "What is Metformin?",
+    answer: "Repurposed longevity drugs, such as metformin and rapamycin, refer to medications that were developed and approved for one disease, and later found to also be useful as longevity medications. Resaerch on these drugs as longevity treatments is larger new, and often still ongoing, but they have shown promise in extending lifespan and healthspan in various studies."
+  }, 
 ];
 
 const PredefinedQA: React.FC = () => {
